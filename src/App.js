369333dee1a67458e77780f7315cf59d25dc675f
Andy Chu <aandychutwa@gmail.com>
2018-10-28 00:38:13 +0800

    v4.0 Merge: Search, Carousel, Bing Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { ReactBingmaps } from 'react-bingmaps';
-// import {Button} from "react-bootstrap";
+// import {Carousel} from "react-bootstrap";
+import Slider from 'react-slick';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+import '../node_modules/slick-carousel/slick/slick.css'; 
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,11 +13,17 @@ import InputBase from '@material-ui/core/InputBase';
 import MenuIcon from '@material-ui/icons/Menu';
 import LayerIcon from './image/terrain.svg';
 import LocationFabImage from './image/my_location.svg';
+import BingLogo from './image/bing_logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.counter = 1;
+    this.d = {}
+
+
     this.state = {
       isVisible : true,
       bingmapKey: "Ah0FkX2Gv1-qefM-NeJQi18Mi0pUtir_s6CZacuUOiuQ-jMRg2jKQq08ex3UVoJ5",
@@ -58,24 +68,53 @@ class App extends Component {
       },
       searchInput: "",
       getLocationHandledData: "",
-      directions: {
-        "renderOptions": {"itineraryContainer": "itineraryContainer" },
-        "requestOptions": {"routeMode": "driving", "maxRoutes": 2},
-        "wayPoints": [
-              {
-                address: 'Chennai, Tamilnadu'
-              },
-              {
-                address: 'Salem, Tamilnadu'
-              }
-            ]
-      },
+      directions: this.d,
+
+    //   directions: {
+    //     "renderOptions": {"itineraryContainer": "itineraryContainer" },
+    //     "requestOptions": {"routeMode": "driving", "maxRoutes": 2},
+    //     "wayPoints": [
+    //           {
+    //             address: 'Chennai, Tamilnadu'
+    //           },
+    //           {
+    //             address: 'Salem, Tamilnadu'
+    //           }
+    //         ]
+    //   },
       mapOptions: {
           showDashboard: false
       }
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
   changeState(){
+    
+    if(this.counter === 0){
+        this.d = {};
+        this.counter = 1;
+        console.log(this.d);
+        this.setState({isVisible: false,
+          mapOptions:{
+            showDashboard: false
+          }
+        });
+      }else{
+        this.d = {
+          "inputPanel": "inputPanel",
+          "renderOptions": {"itineraryContainer": "printoutPanel" },
+          "requestOptions": {"routeMode": "driving", "maxRoutes": 2},
+        };
+        this.counter = 0;
+        console.log(this.d);
+        this.setState({isVisible: true,
+          mapOptions:{
+            showDashboard: false
+          }
+        });
+      }
+
     this.setState({
       infoboxes : [
         {
@@ -123,32 +162,44 @@ class App extends Component {
         "location": [[13.0827, 80.2707],[13.0527, 80.2707]],
         "option": { strokeColor: 'red', strokeThickness: 10, strokeDashArray: [1, 2, 5, 10] }
       },
-      directions: {
-        "inputPanel": "inputPanel",
-        "renderOptions": {"itineraryContainer": "itineraryContainer" },
-        "requestOptions": {"routeMode": "driving", "maxRoutes": 2},
-        "wayPoints": [
-              {
-                address: 'Chennai, Tamilnadu'
-              },
-              {
-                address: 'Salem, Tamilnadu'
-              },
-              {
-                address: 'Coimbatore, Tamilnadu'
-              }
-            ]
-      },
+      directions: this.d,
+    //   directions: {
+    //     "inputPanel": "inputPanel",
+    //     "renderOptions": {"itineraryContainer": "itineraryContainer" },
+    //     "requestOptions": {"routeMode": "driving", "maxRoutes": 2},
+    //     "wayPoints": [
+    //           {
+    //             address: 'Chennai, Tamilnadu'
+    //           },
+    //           {
+    //             address: 'Salem, Tamilnadu'
+    //           },
+    //           {
+    //             address: 'Coimbatore, Tamilnadu'
+    //           }
+    //         ]
+    //   },
       mapOptions: {
         showDashboard: false
       }
     })
+    this.handleSubmit();
   }
   handleSubmit(event){
-    if(this.state.searchInput !== null && this.state.searchInput !== ""){
+
+    console.log("in submit");
+    
+    console.log(this.state.searchInput);
+    console.log(str);
+    // console.log(this.state.boundary.location[0]);
+    // console.log(this.state.boundary.location[1]);
+
+    var str = document.getElementById("inputBase").value
+    console.log(str)
+    if(str !== null && str !== ""){
       this.setState({
         boundary: {
-          "search" : this.state.searchInput,
+          "search" : str,
           "polygonStyle" :{
             fillColor: 'rgba(161,224,255,0.4)',
             strokeColor: '#a495b2',
@@ -161,9 +212,31 @@ class App extends Component {
         mapOptions: {
           showDashboard: false
         }
+        // searchInput: str
       })
     }
-    event.preventDefault();
+
+
+    // event.preventDefault();
+    // if(this.state.searchInput !== null && this.state.searchInput !== ""){
+    //   this.setState({
+    //     boundary: {
+    //       "search" : this.state.searchInput,
+    //       "polygonStyle" :{
+    //         fillColor: 'rgba(161,224,255,0.4)',
+    //         strokeColor: '#a495b2',
+    //         strokeThickness: 2
+    //       },
+    //       "option":{
+    //         entityType: 'PopulatedPlace'
+    //       }
+    //     },
+    //     mapOptions: {
+    //       showDashboard: false
+    //     }
+    //   })
+    // }
+    // event.preventDefault();
   }
   GetLocationHandled(location){
     this.setState({
@@ -173,27 +246,76 @@ class App extends Component {
   GetEventHandled(callbackData){
     console.log(callbackData);
   }
+
+  
   render() {
+    var settings = {
+        className: "CarouselContainer",
+        centerMode: true,
+        centerPadding: '4px',
+        arrows: false,
+        // fade: true,
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        lazyLoad: true,
+    };
+
     return (
       <div>
         <div class="map-one">
             <div >
             <AppBar style={styles.AppBarContainer}>
                 <Toolbar style={styles.Toolbar}>
-                    <InputBase style={styles.SearchBarContainer} placeholder="Input Destination" />
+                    {/* <Button onClick={this.handleSubmit.bind(this)} style={styles.LayerButton}></Button> */}
+                    <InputBase id="inputBase" style={styles.SearchBarContainer} placeholder="Input Destination" />
+                    <Button onClick={this.changeState.bind(this)} style={styles.BingButton}></Button>
                     <Button style={styles.LayerButton}></Button>
                 </Toolbar>
             </AppBar>
             </div>
 
+            <div>
+            
+            <Button onClick={()=>{this.setState({isVisible:!this.state.isVisible})}} style={styles.changeStateContainer2}>
+                {this.state.isVisible ? "Hide" : "Show"}
+            </Button>
+            </div> 
+
             <ReactBingmaps
             id = "nine" 
             className = "customClass"
+            center = {[121.00, 24.00]}
             bingmapKey = {this.state.bingmapKey}
             mapOptions = {this.state.mapOptions}
+            directions = {this.state.directions}
+            boundary = {this.state.boundary}
             >
             </ReactBingmaps>
-            <Button variant="fab" style={styles.MyLocationContainer}></Button>
+            {this.state.isVisible && <div className="direction-container">
+                <div className="input-panel" id='inputPanel'></div>
+                <div className="itinerary-container" id='itineraryContainer'></div>
+              </div>}
+            {/* <Button variant="fab" style={styles.MyLocationContainer}></Button> */}
+            <Slider {...settings} >
+            <div align="center">
+                <Button variant="fab" style={styles.CarouselMid}></Button>
+            </div>
+            <div align="center">
+                <Button variant="fab" style={styles.CarouselLeft}></Button>
+            </div>
+            <div align="center">
+                <Button variant="fab" style={styles.CarouselRight}></Button>
+            </div>
+            {/* <div align="center">
+                <Button variant="fab" style={styles.CarouselLeft}></Button>
+            </div> */}
+            <div align="center">
+                <Button variant="fab" style={styles.CarouselRight}></Button>
+            </div>
+            </Slider>
         </div>   
       </div>
     );
@@ -201,48 +323,91 @@ class App extends Component {
 }
 
 const styles = {
-  MyLocationContainer: {
-    backgroundImage: `url(${LocationFabImage})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    position:'absolute',
-    bottom:80,
-    right:20
-  },
+    MyLocationContainer: {
+        backgroundImage: `url(${LocationFabImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+    },
 
-  AppBarContainer: {
-    backgroundColor: 'transparent',
-    color: '#3B8283',
-    boxShadow: '0px 0px 0px',
-    paddingTop: 10
-  },
+    AppBarContainer: {
+        backgroundColor: 'transparent',
+        color: '#3B8283',
+        boxShadow: '0px 0px 0px',
+        paddingTop: 10
+    },
 
-  LayerButton: {
-    backgroundColor: '#3B8283',
-    opacity: 0.8,
-    backgroundImage: `url(${LayerIcon})`,
-    // backgroundSize: '10%',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    // boxShadow: `0px 2px 0px #3B8283`,
-    borderRadius: 0,
-    border: 'solid #3B8283 2px',
-    borderBottom: 0,
-    padding: 8,
-    width: 20
-  },
+    LayerButton: {
+        backgroundColor: '#3B8283',
+        opacity: 0.8,
+        backgroundImage: `url(${LayerIcon})`,
+        // backgroundSize: '10%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        // boxShadow: `0px 2px 0px #3B8283`,
+        borderRadius: 0,
+        border: 'solid #3B8283 2px',
+        borderBottom: 0,
+        padding: 8,
+        width: 20
+    },
 
-  SearchBarContainer: {
-    backgroundColor: '#FFFFFF',
-    opacity: 0.8,
-    textColor: '#3B8283',
-    // boxShadow: `0px 2px 0px #3B8283`,
-    textAlign: 'center',
-    paddingLeft: 12,
-    width: '90%',
-    height: '36px'
-  }
+    SearchBarContainer: {
+        backgroundColor: '#FFFFFF',
+        opacity: 0.8,
+        textColor: '#3B8283',
+        // boxShadow: `0px 2px 0px #3B8283`,
+        textAlign: 'center',
+        paddingLeft: 12,
+        width: '90%',
+        height: '36px'
+    },
 
+    BingButton: {
+        backgroundImage: `url(${BingLogo})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '70% 50%',
+        backgroundSize: '35%',
+        backgroundColor: '#FFFFFF',
+        opacity: 0.8,
+        borderRadius: 0
+    },
+    changeStateContainer2: {
+        position:'absolute',
+        bottom:-100,
+        right:180
+    },
+
+    CarouselMid: {
+        backgroundImage: `url(${LayerIcon})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        boxShadow: '0px 0px 0px',
+        // margin: '0 auto',
+        // marginRight: 'auto'
+        // position:'absolute',
+        // bottom:80,
+        // left: 150
+    },
+
+    CarouselLeft: {
+        backgroundImage: `url(${LocationFabImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        boxShadow: '0px 0px 0px',
+        // position:'absolute',
+        // bottom:80,
+        // left: 100
+    },
+
+    CarouselRight: {
+        backgroundImage: `url(${LocationFabImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        boxShadow: '0px 0px 0px',
+        // position:'absolute',
+        // bottom:80,
+        // left: 200
+    }
 }
 
 export default App;
